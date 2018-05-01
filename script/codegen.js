dfe8fae5f9ffe11d8447194fc60877e0e77fbdf9
daftspaniel <daftspaniel@gmail.com>
2018-05-01 20:46:17 +0100

    Initial ASM display routine.

--- script/codegen.js
+++ script/codegen.js
@@ -22,7 +22,21 @@ function constructFcb() {
     const noval = 60;	//hex
     getById("fcbs").value = "";
     getById("fcbdata").style.display = "block";
-    var fcbval, charval;
+    let fcbval, charval;
+    let fullASMCode = "        ORG $4E21\r\n" +
+        "        PSHS X,Y,U,A,B\r\n" +
+        "        \r\n" +
+        "        LDX #$400\r\n" +
+        "        LDY #TSB\r\n" +
+        "        \r\n" +
+        "DRAW    LDD ,Y++\r\n" +
+        "        STD ,X++\r\n" +
+        "        CMPX #$600\r\n" +
+        "        BNE DRAW\r\n" +
+        "        \r\n" +
+        "EXIT    PULS X,Y,U,A,B\r\n" +
+        "        RTS ; EXIT PROGRAM\r\n\r\n";
+    let data = '';
 
     for (var j = 0; j < 16; j++) {
         var fcbline = String.fromCharCode(9) + "fcb" + String.fromCharCode(9);
@@ -39,9 +53,11 @@ function constructFcb() {
             fcbline += "$" + fcbval + ","
         }
         fcbline = fcbline.substr(0, fcbline.length - 1) + String.fromCharCode(13);
-        getById("fcbs").value += fcbline
-
+        data += fcbline;
     }
+    data = 'TSB    ' + data.substring(0);
+
+    getById("fcbs").value = fullASMCode + data;
 }
 
 function downloadBasic() {
